ec38928719038a938ce53526874a6d687aa76fff
Leo <muneneboizkaruga@gmail.com>
2025-06-14 21:04:12 +0300

    fixes

--- app.py
+++ app.py
@@ -105,6 +105,6 @@ async def summarize(request: SummarizationRequest):
     
 # ---------- Entry Point ----------
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+#if __name__ == "__main__":
+ #   import uvicorn
+ #   uvicorn.run(app, host="0.0.0.0", port=7860)
